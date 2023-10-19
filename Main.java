285c8e13e0bb05167beab2e75688849a20c61752
Sarah Vanderei <142837608+sarahVicki@users.noreply.github.com>
2023-10-19 12:02:21 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Main {
+  public static void main(String[] args) {
+    ArrayList<Produto> produto = new ArrayList<>();
+    produto.add();
+    
+    System.out.println(produto);
+
+    Carrinho carrinho = new Carrinho();
+    carrinho.Processamento();
+  }
+}
