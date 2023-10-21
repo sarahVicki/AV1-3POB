57c51a43df57fe9a65b5972f4294a72de783f049
Sarah Vanderei <142837608+sarahVicki@users.noreply.github.com>
2023-10-21 01:30:57 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,9 @@ import java.util.ArrayList;
 
 class Main {
   public static void main(String[] args) {
-    ArrayList<Produto> produto = new ArrayList<>();
-    produto.add();
     
-    System.out.println(produto);
-
-    Carrinho carrinho = new Carrinho();
-    carrinho.Processamento();
+    Menu menu = new Menu();
+    menu.processamento();
+    
   }
 }
